4e6dffc7d6e463554e24e5cec9769c757b14689a
Sachit Bansal <sachitbansal2006@gmail.com>
2025-08-01 18:41:43 +0530

    first commit

---
+++ step1/1.1/io.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+    int num;
+    cin >> num;
+    cout << "You entered: " << num << endl;
+    return 0;
+}
