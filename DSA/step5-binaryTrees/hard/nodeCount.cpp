8e56439c1cdaed5770dfb1d9f4401edddf15ea8c
Sachit Bansal <sachitbansal2006@gmail.com>
2025-12-31 22:06:15 +0530

    dsa++

--- DSA/step5-binaryTrees/hard/nodeCount.cpp
+++ DSA/step5-binaryTrees/hard/nodeCount.cpp
@@ -11,7 +11,88 @@
  */
 class Solution {
 public:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
 
+    void anss(TreeNode* root, int &ans){
+        if (root) {
+            ans++;
+            anss(root->right, ans);
+            anss(root->left, ans);
+        } else return;
+        
+    }
+    int countNodes(TreeNode* root) {
+        int ans = 0;
+        anss(root, ans);
+        return ans;
+    }
+};
+
+    void anss(TreeNode* root, int &ans){
+        if (root) {
+            ans++;
+            anss(root->right, ans);
+            anss(root->left, ans);
+        } else return;
+        
+    }
+    int countNodes(TreeNode* root) {
+        int ans = 0;
+        anss(root, ans);
+        return ans;
+    }
+};
+
+    void anss(TreeNode* root, int &ans){
+        if (root) {
+            ans++;
+            anss(root->right, ans);
+            anss(root->left, ans);
+        } else return;
+        
+    }
+    int countNodes(TreeNode* root) {
+        int ans = 0;
+        anss(root, ans);
+        return ans;
+    }
+};
     void anss(TreeNode* root, int &ans){
         if (root) {
             ans++;
