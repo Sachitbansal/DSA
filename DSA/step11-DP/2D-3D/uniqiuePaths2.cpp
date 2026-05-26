ff955b9fe296e6851d22d50e9b766b39d61bc194
Sachitbansal <sachitbansal2006@gmail.com>
2026-05-26 15:38:58 +0530

    dsa++

---
+++ DSA/step11-DP/2D-3D/uniqiuePaths2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        if (obstacleGrid[m-1][n-1] == 1 || obstacleGrid[0][0] == 1) return 0;
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++) {
+                if (j==0 && i == 0) continue;
+                if (obstacleGrid[i][j] == 1) {dp[i][j] = 0; continue;}
+                int left = 0;
+                int right = 0;
+
+                if (i > 0) left = dp[i-1][j];
+                if (j > 0) right = dp[i][j-1];
+
+                dp[i][j] = left + right;
+            }
+        }
+        return dp[m-1][n-1];
+
+    }
+};
